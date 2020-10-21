e68b2a1a55095bb37d39569b61a2ce637f551e99
Normando Ali Zubia Hernández <nzubia@blacklabs.mx>
2020-10-20 20:16:47 -0600

    Bootstrap added to project

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Plugins
+import '@/plugins/boostrap.plugin';
+
 Vue.config.productionTip = false
 
 new Vue({
